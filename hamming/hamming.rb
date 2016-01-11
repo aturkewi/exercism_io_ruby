84cdbbad9c7a434bc90451afc51ce00a83b165cd
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-01-11 09:37:27 -0500

    initial commit

---
+++ hamming/hamming.rb
@@ -0,0 +1,20 @@
+class Hamming
+
+  VERSION = 1
+
+  def self.compute(string1,string2)
+    raise ArgumentError if string1.length != string2.length
+    distance = 0
+    dna_array1 = string1.split('')
+    dna_array2 = string2.split('')
+    dna_array1.each.with_index do |nucleotide, index|
+      distance += 1 if nucleotide != dna_array2[index]
+    end
+    distance
+  end
+
+end
+
+class ArgumentError < StandardError
+
+end
