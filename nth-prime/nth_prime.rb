bcf4848b0d0d44919986513f54769dac2f182cfc
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-02-21 13:15:30 -0500

    Refactored

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -1,32 +1,26 @@
 class Prime
 
-
   def self.nth(n)
     raise ArgumentError if n <= 0
     primes=[2]
-    i=2
-    until primes.length == n
-      i+=1
-      is_prime = true
-      primes.each do |prime|
-        if i%prime == 0
-          is_prime = false
-        end
-      end
-      primes << i if is_prime # ?(primes, i)
-    end
+    itterate_to_nth(primes, n)
     primes.last
   end
 
   private
 
-  # def self.is_prime?(primes, i)
-  #   is_prime = true
-  #   primes.each do |prime|
-  #     if i%prime == 0
-  #       is_prime = false
-  #     end
-  #   end
-  # end
+  def self.itterate_to_nth(primes, n)
+    i=2
+    until primes.length == n
+      i+=1
+      primes << i if is_prime?(primes, i)
+    end
+  end
+
+  def self.is_prime?(primes, i)
+    is_prime = true
+    primes.each {|prime| is_prime = false if i%prime == 0}
+    is_prime
+  end
 
 end
